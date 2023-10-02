11b76cb97d28fad474caf4b1dd93cdc201b54d25
Thiago Cavalcante <th-fernandes@hotmail.com>
2023-10-02 17:29:16 -0300

    teste

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 
+let icon = document.getElementById('btn-icon')
+icon.addEventListener('click', () => {
+    showMenu();
+})
 
 function showMenu(){
     nav = document.querySelector('nav')
 
     if(nav.classList.contains('open')){
         nav.classList.remove('open')
-
+        icon.innerHTML= `menu`
     }else{
         nav.classList.add('open')
+        icon.innerHTML= `close`
+       
     }
     
 }
