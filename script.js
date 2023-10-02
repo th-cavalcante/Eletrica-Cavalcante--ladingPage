9675af1511c559223b60974aeb790564e924fa0f
Thiago Cavalcante <th-fernandes@hotmail.com>
2023-10-02 12:34:39 -0300

    teste

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
 
-// Função para exibir o slide atual
-function showSlide() {
-  slides.forEach((slide) => {
-    slide.classList.remove('active');
-  });
 
-  slides[currentSlide].classList.add('active');
+function showMenu(){
+    nav = document.querySelector('nav')
+
+    if(nav.classList.contains('open')){
+        nav.classList.remove('open')
+
+    }else{
+        nav.classList.add('open')
+    }
+    
 }
-
-// Função para alternar automaticamente os slides
-function autoPlay() {
-  currentSlide++;
-  if (currentSlide > slides.length - 1) {
-    currentSlide = 0;
-  }
-  showSlide();
-}
-
-// Definir intervalo de tempo para alternar os slides (a cada 5 segundos)
-setInterval(autoPlay, 2000);
-
-// Exibir o primeiro slide ao carregar a página
-showSlide();
